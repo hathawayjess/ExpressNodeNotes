cdb30362e10560d5e58d0737851427d327884416
Jessica Hathaway <hathaway.jess@gmail.com>
2016-06-17 15:22:54 -0600

    initial commit

---
+++ session-object.js
@@ -0,0 +1,10 @@
+Session {
+  cookie:
+   { path: '/',
+     _expires: null,
+     originalMaxAge: null,
+     httpOnly: true },
+  currentUser:
+   { name: 'Preston McNeil',
+     password: 'password1',
+     friends: [ 'Lindsey Mayer', 'Terri Ruff' ] } }
